863625b257dca48b04bfe313f8c3d97ccbeabcb2
BlackOwl <107190154+blackowltr@users.noreply.github.com>
2024-04-24 17:06:09 +0300

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,13 +24,13 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/blackowltr/blackowltr.github.io/',
+          editUrl: 'sitemap.xml',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/blackowltr/blackowltr.github.io/',
+            'https://github.com/blackowltr/blackowltr.github.io/tree/main',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
